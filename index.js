eab7d6e18fc91844e5a4bfc8264c00670a2e48db
Thomas <thomas.laine25+1@gmail.com>
2021-03-17 11:07:21 +0100

    refactored code and allowed user to resize grid

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-// globals
-let gridSize = 16;
-const divToCreate = gridSize * gridSize;
-
-//helper
+//HELPER
 const getColor = () => {
   return Math.floor(Math.random() * 16777215).toString(16);
 };
 
-// program logic
+const colorGrid = () => {
+  const square = document.getElementsByClassName("grid-item");
+  for (let i = 0; i < square.length; i++) {
+    square[i].addEventListener("mouseenter", () => {
+      square[i].style.cssText = "background-color: #" + getColor();
+    });
+  }
+};
+
+const fillGrid = (size) => {
+  const gridSquare = size * size;
+  for (let i = 0; i < gridSquare; i++) {
+    const item = document.createElement("div");
+    item.classList.add("grid-item");
+    container.appendChild(item);
+  }
+  container.style.cssText = "grid-template-columns: repeat(" + size + ", 1fr);";
+};
+
+const delGrid = (elementName) => {
+  [...document.getElementsByClassName(elementName)].map((n) => n && n.remove());
+};
+
+// LOGIC
 const container = document.querySelector(".container");
+fillGrid(16);
+colorGrid();
 
 const button = document.querySelector("button");
+
 button.addEventListener("click", () => {
   const userEntry = window.prompt("Choose a grid size", "Max. value 100");
   if (userEntry > 100) {
@@ -20,20 +42,7 @@ button.addEventListener("click", () => {
     alert("You must enter a number only");
     const userEntry = window.prompt("Choose a grid size", "Max. value 100");
   }
+  delGrid("grid-item");
+  fillGrid(userEntry);
+  colorGrid();
 });
-
-container.style.cssText =
-  "grid-template-columns: repeat(" + gridSize + ", 1fr);";
-
-for (let i = 0; i < divToCreate; i++) {
-  const item = document.createElement("div");
-  item.classList.add("grid-item");
-  container.appendChild(item);
-}
-
-const square = document.getElementsByClassName("grid-item");
-for (let i = 0; i < square.length; i++) {
-  square[i].addEventListener("mouseenter", () => {
-    square[i].style.cssText = "background-color: #" + getColor();
-  });
-}
